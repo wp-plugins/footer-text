a0e38637bf629d2bd339ce4fe9b9e7c9d1643747
bungeshea <bungeshea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-02 10:12:40 +0000

    Release version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/footer-text/trunk@690561 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ footer-text.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * Plugin Name: Footer Text
+ * Plugin URI: http://bungeshea.com/plugins/footer-text/
+ * Description: Allow changing of the theme footer text easily from the dashboard
+ * Author: Shea Bunge
+ * Author URI: http://bungeshea.com
+ * License: MIT
+ * License URI: http://opensource.org/licenses/mit-license.php
+ * Version: 1.0
+ */
+
+/** Dashboard Administration Menu ********************************************/
+
+/**
+ * Add the footer text options page to
+ * the 'Appearance' dashboard menu
+ *
+ * @uses add_theme_page() To register the new sub-menu
+ *
+ * @return void
+ *
+ * @since 1.0
+ */
+function add_footer_text_options_page() {
+	$theme_page = add_theme_page(
+		__( 'Footer Text', 'footer-text' ), // Name of page
+		__( 'Footer Text', 'footer-text' ), // Label in menu
+		'edit_theme_options',               // Capability required
+		'footer-text',                      // Menu slug, used to uniquely identify the page
+		'render_footer_text_options_page'   // Function that renders the options page
+	);
+}
+add_action( 'admin_menu', 'add_footer_text_options_page' );
+
+/**
+ * Display the footer text options page
+ * and save posted text to the database
+ *
+ * @uses update_option() To save the text to the database
+ * @uses screen_icon() To display the dashboard menu icon
+ * @uses wp_editor() For a visual editor
+ * @uses get_option() To retrieve the current text from the database
+ * @uses submit_button() To generate a form submit button
+ *
+ * @return void
+ *
+ * @since 1.0
+ */
+function render_footer_text_options_page() {
+
+	if ( isset( $_POST['footer_text'] ) )
+		update_option( 'theme_footer_text', stripslashes( $_POST['footer_text'] ) );
+
+	?>
+	<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2><?php _e( 'Footer Text', 'footer-text' ); ?></h2>
+
+
+		<form method="post" action="" style="margin: 20px 0;">
+			<?php
+				wp_editor( get_option( 'theme_footer_text', '' ), 'footer_text' );
+				submit_button();
+			?>
+		</form>
+	</div>
+	<?php
+}
+
+/** Template Tags ******************************************************/
+
+/**
+ * Fetches the footer text from the database
+ * with formatting functions applied
+ *
+ * @param string $default What to use if no footer text is set
+ * @return string The formatted footer text
+ *
+ * @since 1.0
+ */
+function get_footer_text( $default = '' ) {
+
+	// retrieve the footer text from the database
+	$footer_text = get_option( 'theme_footer_text', $default );
+
+	// format the text and apply shortcodes
+	$footer_text = do_shortcode( apply_filters( 'the_content', $footer_text ) );
+
+	// filter and return the text
+	return apply_filters( 'get_footer_text', $footer_text );
+}
+
+/**
+ * Retrieves the footer text and displays it if it is set
+ * Nothing is displayed if the footer text is not set
+ *
+ * @uses get_footer_text() To retrieve the footer text
+ *
+ * @param string $before The text to display before the footer text
+ * @param string $after The text to display after the footer text
+ * @param string $default What to display if no text is set
+ * @return void
+ *
+ * @since 1.0
+ */
+function footer_text( $before = '', $after = '', $default = '' ) {
+
+	$footer_text = get_footer_text( $default );
+	$output = ( empty( $footer_text ) ? '' : $before . $footer_text . $after );
+
+	echo $output;
+}
+
+/** Shortcodes ********************************************************/
+
+/**
+ * Returns a formatted link to
+ * the current page's permalink
+ *
+ * @uses get_permalink()
+ *
+ * @return string
+ *
+ * @since 1.0
+ */
+function footer_text_shortcode_permalink() {
+	$label = ( isset( $atts ) ? (string) $atts : get_permalink() );
+	return sprintf ( '<a href="%1$s">%2$s</a>', get_permalink(), $label );
+}
+
+/**
+ * Returns the date when the current page
+ * was last modified
+ *
+ * @uses the_modified_date()
+ *
+ * @return string
+ *
+ * @since 1.0
+ */
+function footer_text_shortcode_last_modified() {
+	return the_modified_date( 'd/m/Y', '<time>', '</time>', false );
+}
+
+/**
+ * Returns the current year, ideal for
+ * a copyright notice
+ *
+ * @return string
+ *
+ * @since 1.0
+ */
+function footer_text_shortcode_current_year() {
+	return '<time>' . date( 'Y' ) . '</time>';
+}
+
+/**
+ * Returns an array of the shortcode tags to run
+ * on the footer text, paired with their callbacks
+ *
+ * @return array
+ *
+ * @since 1.0
+ */
+function get_footer_text_shortcode_tags() {
+	return apply_filters( 'footer_text_shortcode_tags', array(
+		'last_modified' => 'footer_text_shortcode_last_modified',
+		'page_link' => 'footer_text_shortcode_permalink',
+		'year' => 'footer_text_shortcode_current_year',
+	) );
+}
+
+/**
+ * Registers our shortcodes with WordPress
+ *
+ * @uses add_shortcode()
+ *
+ * @since 1.0
+ */
+function add_footer_text_shortcodes() {
+	$shortcode_tags = get_footer_text_shortcode_tags();
+
+	foreach ( $shortcode_tags as $shortcode_tag => $callback )
+		add_shortcode( $shortcode_tag, $callback );
+}
+add_action( 'init', 'add_footer_text_shortcodes' );
+
+/**
+ * Removes our custom shortcodes from the post content
+ * so they don't interfere with anything
+ *
+ * @link http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
+ *
+ * @since 1.0
+ */
+function footer_text_post_content_remove_shortcodes( $content ) {
+
+	/* Retrieve an array of all the shortcode tags */
+	$shortcode_tags = get_footer_text_shortcode_tags();
+
+	/* Loop through the shortcodes and remove them */
+	foreach ( $shortcode_tags as $shortcode_tag => $callback )
+		remove_shortcode( $shortcode_tag );
+
+	/* Return the post content */
+	return $content;
+}
+add_filter( 'the_content', 'footer_text_post_content_remove_shortcodes', 0 );
+
+/**
+ * Adds our shortcodes back to the post content when it's safe
+ *
+ * @see http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
+ *
+ * @since 1.0
+ */
+function footer_text_post_content_add_shortcodes( $content ) {
+
+	/* Add the original shortcodes back. */
+	add_footer_text_shortcodes();
+
+	/* Return the post content. */
+	return $content;
+}
+add_filter( 'the_content', 'footer_text_post_content_add_shortcodes', 99 );
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Footer Text ===
+Contributors: bungeshea
+Donate link: http://bungeshea.com/donate/
+Tags: front-end, footer, admin, dashboard, visual, theme, footer text, shortcodes, template tags
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: MIT
+License URI: license.txt
+
+Allow changing of the theme footer text easily from the dashboard
+
+== Description ==
+
+Provides an interface in the dashboard, similar to the post edit screen, that allows you to easily change the text displayed in the footer on the front-end. After installing the plugin, add the `footer_text()` template tag to your `footer.php` theme template where you want the text to display. For more options, see the [FAQ](http://wordpress.org/extend/plugins/footer-text/faq).
+
+You can use these shortcodes in the footer text editor:
+
+* `[last_modified]` the date that the current page was last modified on
+* `[page_link]` the full permalink of the current page, formatted
+* `[year]` the current year eg: 2013
+
+Visit the [plugin homepage](http://bungeshea.com/plugins/footer-text/), or contribute to its development at [GitHub](https://github.com/bungeshea/footer-text/).
+
+== Installation ==
+
+1. Upload `footer-text.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Visit *Appearance > Footer Text* to write your awesome footer text
+1. Place the `footer_text()` template tag somewhere in your theme where you want the text displayed
+1. Visit site. Observe.
+
+If you'd like to bundle this file with a theme so users don't need to install this plugin as well, copy the `footer-text.php` file to your theme's directory, and include this code in your theme's `functions.php`:
+
+    include_once( get_template_directory_uri() . '/footer-text.php' );
+
+Then use the `footer_text()` template tag in your theme templates.
+
+== Frequently Asked Questions ==
+
+= How can I display the footer text in my theme? =
+You can use the `footer_text()` function to display the footer text, or the `get_footer_text()` function to return it for use in PHP. These template tags should generally be used in the `footer.php` file of your theme.
+
+However, if the plugin isn't active, the template tag will result in an error. To solve this, you can use this code instead:
+
+   <?php if ( function_exists( 'footer_text' ) ) footer_text(); ?>
+
+The `get_footer_text()` function returns the formatted footer text and accepts one parameter: `$default`, which will be returned if no text is set
+
+The `footer_text()` function outputs the formatted footer text and accepts three parameters: `$before`, `$after` and `$default`. `$before` will be outputted *before* the text, `$after` will be outputted *after* the text, and `$default` will be used instead of the text is none is set. If no text is set `$default` is empty, nothing will be displayed.
+
+= Can I bundle this plugin with my theme so users don't need to install this plugin? =
+
+Sure thing! Just follow the instructions in the [Installation](http://wordpress.org/extend/plugins/footer-text/installation) section.
+
+== Screenshots ==
+
+1. Editing the footer text in the dashboard
+2. Previewing the footer text on the Twenty Eleven theme
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
